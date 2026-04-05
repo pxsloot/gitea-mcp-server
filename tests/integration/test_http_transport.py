a71fa14de23762b666052fff387b9b49af9c089b
Peter S. <dev2@home.lan>
2026-04-05 21:35:52 +0200

    Revert "Merge pull request '[Fix] Resolve lint issues in HTTP transport implementation' (#95) from fix/94-lint-http-transport into main"
    This reverts commit 050fa0ac1681d8d058951dc93015c1095911dfd5, reversing
    changes made to f9b2a682c1fbe91a055b7ec2deb48907d8ae6a70.

--- tests/integration/test_http_transport.py
+++ tests/integration/test_http_transport.py
@@ -5,6 +5,7 @@ from unittest.mock import patch
 
 import pytest
 import respx
+from starlette.testclient import TestClient
 
 from gitea_mcp_server.client import GiteaClient
 from gitea_mcp_server.config import Config
@@ -14,7 +15,7 @@ from gitea_mcp_server.server import create_mcp_server
 class SimpleHTTPConfig:
     """Simple config stub for HTTP transport tests."""
 
-    def __init__(  # noqa: PLR0913
+    def __init__(
         self,
         url="https://git.example.com",
         token="test_token",

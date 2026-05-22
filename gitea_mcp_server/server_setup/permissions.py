1ca75e87736b18f26f06faeade2688196c17ffb8
Peter S. <dev2@home.lan>
2026-05-22 23:31:47 +0200

    Fix import formatting in permissions.py (#175)

--- gitea_mcp_server/server_setup/permissions.py
+++ gitea_mcp_server/server_setup/permissions.py
@@ -1,5 +1,8 @@
 """Permission filtering utilities."""
 
-from gitea_mcp_server.tool_filter import filter_resources_by_permissions, filter_tools_by_permissions
+from gitea_mcp_server.tool_filter import (
+    filter_resources_by_permissions,
+    filter_tools_by_permissions,
+)
 
 __all__ = ["filter_resources_by_permissions", "filter_tools_by_permissions"]
